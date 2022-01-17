f93ea8c7c3c465e09b7a23c5cd5dfc9305d0266f
Ömer Faruk Ergin <omerfarukerginn@gmail.com>
2022-01-17 21:48:51 +0300

    First

---
+++ src/test/java/day01/C01_IlkClass.java
@@ -0,0 +1,20 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_IlkClass {
+    public static void main(String[] args) throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        // bir driver objesi olusturdugumuzda bos bir chrome browser acilir
+
+        driver.get("https://www.amazon.com");
+        // driver.get method'u driver'a gidecegi web adresini girmimizi saglar
+
+        Thread.sleep( 3000);
+
+        driver.close();
+        // driver'i kapatir
+    }
+}
